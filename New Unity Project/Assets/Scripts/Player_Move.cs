a7d1ddb281ef41ac7e234c00fad8e05a86f3af71
Thads Michael Egar <ThadsMichaelEgar@users.noreply.github.com>
2018-07-16 20:17:36 -0500

    HealthBar V1
    Health Bar Follows Player. Need to change it so only one canvas is used. And health bar needs fill adjustments.
    
    Player Script needs to be renamed.

--- New Unity Project/Assets/Scripts/Player_Move.cs
+++ New Unity Project/Assets/Scripts/Player_Move.cs
@@ -1,25 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_Move : MonoBehaviour {
-	//stats
-	public int playerHealth;
-
-	//movement
-	public int playerSpeed = 5;
-	public bool facingRight = true;
-	public int playerJumpAmount = 500;
+    //img-----------------------------------
+    public Image HealthBar;
+	//stats---------------------------------
+        //initial (used for calculation)
+	public float startPlayerHealth = 100;
+    public float startPlayerSpeed = 5;
+        //current
+    public float playerHealth;
+    public float playerSpeed;
+    
+    
+    //movement------------------------------
+    public bool facingRight = true;
+	public float playerJumpAmount = 500;
 	public float moveX;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        playerHealth = startPlayerHealth;
+        playerSpeed = startPlayerSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		PlayerMove();
+        //temp check health works
+        minusHealth(1f);
 	}
 
 	void PlayerMove(){
@@ -55,4 +67,22 @@ public class Player_Move : MonoBehaviour {
         localScale.x *= -1;
         transform.localScale = localScale;
 	}
+
+    //--------------------HEALTH MOD/CALCULATION--------------------------
+    void minusHealth(float amount)
+    {
+        playerHealth -= amount;
+        HealthBar.fillAmount = playerHealth / startPlayerHealth;
+
+    }
+
+    void addHealth(float amount)
+    {
+        playerHealth += amount;
+    }
+
+    void killPlayer()
+    {
+        //TODO
+    }
 }
